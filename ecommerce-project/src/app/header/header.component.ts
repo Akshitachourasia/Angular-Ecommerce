16a79dbb0ba7644c0c0680d577ac6f4eaa9515bc
Akshita Chourasia <akshitac.brainerhub@gmail.com>
2024-07-01 17:30:53 +0530

    logout and seller page

--- ecommerce-project/src/app/header/header.component.ts
+++ ecommerce-project/src/app/header/header.component.ts
@@ -10,6 +10,7 @@ import { RouterLink, RouterOutlet , Router} from '@angular/router';
 })
 export class HeaderComponent {
   menuType: string = "default"
+  sellerName: string = ""
   constructor(private route: Router) { }
 ngOnInit(): void {
   this.route.events.subscribe((val: any) => {
@@ -17,6 +18,13 @@ ngOnInit(): void {
       if (localStorage.getItem('seller') && val.url.includes('seller')) {
        console.log("seller logged in")
        this.menuType = "seller"
+       if (localStorage.getItem('seller')) {
+        let sellerStore = localStorage.getItem('seller')
+        let sellarData = sellerStore && JSON.parse(sellerStore)[0]
+        console.log(sellarData.name,"lllllllllllllllllllllllll")
+        this.sellerName = sellarData.name
+      
+      }
       }
       else {
         console.log("seller not logged in")
@@ -25,4 +33,9 @@ ngOnInit(): void {
     }
   })
 }
+logOut(){
+  localStorage.removeItem('seller')
+  this.route.navigate(['/'])
+}
+
 }
