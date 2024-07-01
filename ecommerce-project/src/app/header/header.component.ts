5741121c9c5e8d7da1254c4444b4c2ca60f395f0
Akshita Chourasia <akshitac.brainerhub@gmail.com>
2024-07-01 16:39:54 +0530

    seller profile

--- ecommerce-project/src/app/header/header.component.ts
+++ ecommerce-project/src/app/header/header.component.ts
@@ -1,13 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-
+import { RouterLink, RouterOutlet , Router} from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterOutlet , RouterLink],
+  imports: [RouterOutlet , RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  menuType: string = "default"
+  constructor(private route: Router) { }
+ngOnInit(): void {
+  this.route.events.subscribe((val: any) => {
+    if (val.url) {
+      if (localStorage.getItem('seller') && val.url.includes('seller')) {
+       console.log("seller logged in")
+       this.menuType = "seller"
+      }
+      else {
+        console.log("seller not logged in")
+        this.menuType = "default"
+      }
+    }
+  })
+}
 }
