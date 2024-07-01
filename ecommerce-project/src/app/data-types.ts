a647ef72f4636e48215f763dd3193c6bb722ec4e
Akshita Chourasia <akshitac.brainerhub@gmail.com>
2024-07-01 19:15:09 +0530

    api integration

--- ecommerce-project/src/app/data-types.ts
+++ ecommerce-project/src/app/data-types.ts
@@ -7,3 +7,11 @@ export interface Login{
     email: string;
     password: string;
 }
+export interface Product{
+    name: string;
+    category: string;
+    color: string;
+    price: number;
+    description: string;
+    image: string;
+}
--- ecommerce-project/src/app/seller-add-product/seller-add-product.component.ts
+++ ecommerce-project/src/app/seller-add-product/seller-add-product.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ProductService } from '../services/product.service';
+import { Product } from '../data-types';
 
 @Component({
   selector: 'app-seller-add-product',
@@ -10,7 +12,17 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './seller-add-product.component.css'
 })
 export class SellerAddProductComponent {
-  submit(data: object) {
-    console.log(data);
+  constructor(private product: ProductService) { }
+addProductMessage: string | undefined
+  submit(data: Product) {
+    this.product.addProduct(data).subscribe((result) => {
+      console.log(result)
+      if (result) {
+       this.addProductMessage = "Product added successfully"
+      }
+      setTimeout(() => {
+        this.addProductMessage = undefined
+      }, 3000)
+    })
   }
 }
