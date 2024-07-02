4575c63f790b3b287510536bf2f95761aa2c7294
Akshita Chourasia <akshitac.brainerhub@gmail.com>
2024-07-02 18:48:28 +0530

    prefilled form

--- ecommerce-project/src/app/seller-update-product/seller-update-product.component.ts
+++ ecommerce-project/src/app/seller-update-product/seller-update-product.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule} from '@angular/common';
 import { Product } from '../data-types';
 import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product.service';
 @Component({
   selector: 'app-seller-update-product',
   standalone: true,
@@ -10,6 +12,18 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './seller-update-product.component.css'
 })
 export class SellerUpdateProductComponent {
+  constructor( private route: ActivatedRoute , private Product : ProductService) { }
+  productData: Product | undefined
+ngOnInit(): void {
+  let productId = this.route.snapshot.paramMap.get('id')
+  productId && this.Product.getProduct(productId).subscribe((data) => {
+    this.productData = data
+    console.log(data)
+  })
+
+
+
+}
 submit(product: Product) {
   console.log(product)
 }
