fcc3fd45de700173d1b49f1baf1fdca4446b55e3
Akshita Chourasia <akshitac.brainerhub@gmail.com>
2024-07-03 11:42:47 +0530

    display product

--- ecommerce-project/src/app/home/home.component.ts
+++ ecommerce-project/src/app/home/home.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProductService } from '../services/product.service';
+import { Product } from '../data-types';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgbCarouselModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  popularProducts: undefined | Product[]
+  constructor(private Product: ProductService) { }
+  ngOnInit(): void {
+    this.Product.popularProducts().subscribe((data) => {
+      console.log(data)
+      this.popularProducts = data
+    })
+  }
 
 }
