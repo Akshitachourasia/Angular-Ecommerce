8aaf5a5e8ffbf29271bca9beb80fb9ec2224b8a7
Akshita Chourasia <akshitac.brainerhub@gmail.com>
2024-07-04 18:59:53 +0530

    login form

--- ecommerce-project/src/app/user-auth/user-auth.component.ts
+++ ecommerce-project/src/app/user-auth/user-auth.component.ts
@@ -1,23 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Signup } from '../data-types';
+import { Login, Signup } from '../data-types';
 import { UserService } from '../services/user.service';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-user-auth',
   standalone: true,
-  imports: [CommonModule ,FormsModule ],
+  imports: [CommonModule, FormsModule],
   templateUrl: './user-auth.component.html',
   styleUrl: './user-auth.component.css'
 })
 export class UserAuthComponent {
-  constructor(private user: UserService, private http:HttpClient , private router:Router) { }
-signUp(data: Signup) {
-  this.user.userSignUp(data);
-}
-ngOnInit(): void {
-  this.user.userAuthReload()
-}
+  showLogin = true
+  constructor(private user: UserService, private http: HttpClient, private router: Router) { }
+  signUp(data: Signup) {
+    this.user.userSignUp(data);
+  }
+  ngOnInit(): void {
+    this.user.userAuthReload()
+  }
+
+  login(data: Login) {
+    console.log(data);
+
+  }
+  openSignUp(){
+    this.showLogin=false
+  }
+  openLogin(){
+    this.showLogin=true
+  }
 }
